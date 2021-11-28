4c034c0b39e7e0036541547610e744446ae98934
ManikandanSuryaprakasam <95176608+ManikandanSuryaprakasam@users.noreply.github.com>
2021-11-28 20:08:41 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		// Variable declared in for loop cannot be used outside the block
+		for(int i=1;i<=10;i++) {
+			
+			if(i % 2==1)
+			{
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
